69963bea6b1a9d004d33b5bc283d9a73bc7ad47e
manjot kaur <manjot.kaur97@gmail.com>
2020-04-21 00:27:44 +0530

    donor without time

---
+++ app/src/main/java/com/example/googlemapsdonor/DonationsStatus.java
@@ -0,0 +1,13 @@
+package com.example.googlemapsdonor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonationsStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donations_status);
+    }
+}
