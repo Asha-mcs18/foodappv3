81e621d57e2f159d1c1fc68a5d5b154b9ddb4cb5
manjot kaur <manjot.kaur97@gmail.com>
2020-04-21 19:09:23 +0530

    login register donor

---
+++ app/src/main/java/com/example/googlemapsdonor/DonorActivity.java
@@ -0,0 +1,13 @@
+package com.example.googlemapsdonor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor);
+    }
+}
