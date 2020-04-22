223c33a6ca87ff2bfd6afe76471dd1e3c616b32d
manjot kaur <manjot.kaur97@gmail.com>
2020-04-22 14:51:21 +0530

    donor login register

---
+++ app/src/main/java/com/example/googlemapsdonor/NgoActivity.java
@@ -0,0 +1,13 @@
+package com.example.googlemapsdonor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NgoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngo);
+    }
+}
